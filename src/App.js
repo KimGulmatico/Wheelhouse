1b144983b839c97e2df0e4fe96964b8a9cb660fa
Kim Benedict Gulmatico <kim.gulmatico@kingsland.com>
2018-11-08 14:03:13 +0800

    OK

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import Wheelhouse from './Wheelhouse'
-import Startup from './Startup'
 import AdminW from './AdminWheelhouse'
-import AdminS from './AdminStartup'
 
 class App extends Component{
     render() {
         return(
         <Switch>
-            <Route exact path="/" component={Startup}/>
-            <Route exact path="/Wheelhouse" component={Wheelhouse}/>
-            <Route exact path="/Wheelhouse/Admin" component={AdminW}/>
-            <Route exact path="/Admin" component={AdminS}/>
+            <Route exact path="/" component={Wheelhouse}/>
+            <Route exact path="/Admin" component={AdminW}/>
         </Switch>
         )
     }
